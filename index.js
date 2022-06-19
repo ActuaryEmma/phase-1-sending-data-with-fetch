eb4f9fe0608014422ef6423a5f2438c7cc1a17a3
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-06-19 23:47:50 +0300

    commit tree a16658af5d5f68e6a7f26fb045068b484d95e0a0

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-// Add your code here
 
-function registerSelf() {
-    return fetch("http://guestbook.example.com/register", {
-      "method": "POST",
-      "body": {
-        "firstName": "BYRONIUS KARBITUS MARIS",
-        "registryMessage": "SALVETE AMICI!"
-      }
-    }).then(function(response) {
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({name, email})
+  }) 
+    .then(function(response) {
       return response.json();
-    }).then(function(json) {
-      return json.message;
+    })
+    .then(function(data) {
+      document.body.innerHTML = data["id"]
+    })
+    .catch(function(error) {
+      //alert(error)
+      document.body.innerHTML = error.message
+      //return error
     });
   }
-  
-  function errorSelf() {
-    return fetch("http://guestbook.example.com/register-error", {
-      "method": "POST",
-      "body": {
-        "firstName": "Byron the Poodle",
-        "registryMessage": "Bite!"
-      }
-    }).then(function(response) {
-      return response.json();
-    }).then(function(json) {
-      return json.message;
-    }).catch(function(x) {
-      // console.log(x) // If you want to see the error message that came in
-      return ":(";
-    });
-  }
+console.log(submitData("Jane","habe"))
+
+
